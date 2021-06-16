21c3bd343cc5b7da8db124f62432d1e239d79f01
gengee323 <geng323@gmail.com>
2021-06-16 15:30:48 -0700

    cart

--- src/redux/cartReducer.js
+++ src/redux/cartReducer.js
@@ -1,1 +1,25 @@
-// cart reducer
+// initial state
+const initialState = {
+    cart: []
+  }
+  // action types
+  const SET_CART = "SET_CART"
+  // action builders
+  export function setCart(cart){
+    console.log(cart, 'this is the cart from redux')
+    return {
+      type: SET_CART,
+      payload: cart
+    }
+  }
+  // reducer
+  export default function cartReducer(state = initialState, action){
+    console.log(action.payload)
+    switch(action.type){
+      case SET_CART:
+        return {...state, cart: action.payload}
+      default:
+        return {...state}
+    }
+  }
+  
