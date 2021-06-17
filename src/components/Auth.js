3c1403c450419c81909c8d294516cefa5b6fe652
Noe Pinedo <npine12@gmail.com>
2021-06-17 09:39:28 -0600

    auth component created

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,1 +1,50 @@
 // Auth Js
+import { useState } from 'react'
+import axios from 'axios'
+import { setUser } from '../redux/authReducer'
+import { setCart } from '../redux/cartReducer'
+import { useDispatch } from 'react-redux'
+
+const Auth = (props) => {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const dispatch = useDispatch()
+    const handleRegister = () => {
+        axios.post('/auth/register', { username, password })
+            .then((res) => {
+                dispatch(setUser(res.data))
+                axios.get('/api/cart').then((response) => {
+                    dispatch(setCart(response.data))
+                    props.history.push('/products')
+                })
+            })
+            .catch(err => console.log(err))
+    }
+    const handleLogin = () => {
+        axios.post('/auth/login', { username, password })
+            .then((res) => {
+                console.log(res.data)
+                dispatch(setUser(res.data))
+                axios.get('/api/cart').then((response) => {
+                    dispatch(setCart(response.data))
+                    props.history.push('/products')
+                })
+            })
+            .catch(err => console.log(err))
+    }
+    return (
+
+        <div>
+            <h1>Login/Register</h1>
+            <h5>Username</h5>
+            <input value={username} onChange={(e) => setUsername(e.target.value)} />
+            <h5>Password</h5>
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <button onClick={handleLogin}>Login</button>
+            <button onClick={handleRegister}>Register</button>
+        </div>
+
+    )
+}
+
+export default Auth
