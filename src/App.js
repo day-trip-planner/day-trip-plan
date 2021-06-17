330201c9ff695f29d57ed5657e74d7c568a1ecbd
RobertLynch21 <r.f.lynch@gmail.com>
2021-06-17 11:14:10 -0400

    added routes file, linkes and products file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import routes from './routes';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-<h1>This website is under construction</h1>
-      </header>
+      <Header />
+      {routes}
+      <p>Find the best travel deals on vacation packages when you book on Day Trip Planner. 24/7 Customer Support. Day Trip Planner Rewards. Get the Free Day Trip Planner App. Save up to 50% on Hotels. Verified Guest Reviews. Packages: Save up to 20% Double Points on App. Fast & Secure Booking.</p>
     </div>
   );
 }
