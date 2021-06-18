cf11609e5ad5a9c20ff9299bfbe447a2f3840fa7
kaley forster <55896549+kale-redhead@users.noreply.github.com>
2021-06-18 11:43:14 -0400

    API implementation and added add_product.sql to the db

--- server/controllers/productsCtrl.js
+++ server/controllers/productsCtrl.js
@@ -10,5 +10,19 @@ module.exports = {
             console.log(err)
             res.status(500).send(err)
         })
+    },
+
+    addProducts: (req, res) => {
+        const db = req.app.get('db')
+        const {product_id} = req.params
+    
+        db.products.add_products(product_id, name, price)
+            .then((product) => {
+                res.status(200).send(product)
+            })
+            .catch(err => {
+                console.log(err)
+                res.status(533).send(err)
+            })
     }
 }
