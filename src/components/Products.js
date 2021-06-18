7dd33dc560c3fb53e9557b4f1dbfb8a43dac917c
kaley forster <55896549+kale-redhead@users.noreply.github.com>
2021-06-18 12:12:08 -0400

    Products API work

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,22 +1,58 @@
 import axios from 'axios'
+import {useState} from 'react'
 
 const Products = () => {
-    const options = {
+
+    //car rental information
+    const [location] = useState('')
+    const [date_time_return] = useState('')
+    const [location_return] = useState('')
+    const [location_pickup] = useState('')
+    const [date_time_pickup] = useState('')
+
+    const carLocation = {
         method: 'GET',
         url: 'https://priceline-com-provider.p.rapidapi.com/v1/cars-rentals/locations',
-        params: {name: 'London'},
+        params: {name: {location}},
         headers: {
             'x-rapidapi-key': 'ab419d1536mshec16b6e719f722ep1c42dfjsn74ccdcfb7164',
             'x-rapidapi-host': 'priceline-com-provider.p.rapidapi.com'
         }
     };
 
-    axios.request(options).then(function (response) {
-	    console.log(response.data);
-    })
-    .catch((error) => {
-	    console.log(error);
-    });
+    axios.request(carLocation)
+        .then((response) => {
+	        console.log(response.data);
+        })
+        .catch((error) => {
+	        console.log(error);
+        });
+
+    const carRentals = {
+        method: 'GET',
+        url: 'https://priceline-com-provider.p.rapidapi.com/v1/cars-rentals/search',
+        params: {
+            date_time_return: {date_time_return},
+            location_return: {location_return},
+            location_pickup: {location_pickup},
+            date_time_pickup: {date_time_pickup}
+        },
+        headers: {
+            'x-rapidapi-key': 'ab419d1536mshec16b6e719f722ep1c42dfjsn74ccdcfb7164',
+            'x-rapidapi-host': 'priceline-com-provider.p.rapidapi.com'
+        }
+    };
+      
+    axios.request(carRentals)
+        .then((response) => {
+            console.log(response.data);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+
+    //flight information
+    //hotel information
       
     return(
         <div>
