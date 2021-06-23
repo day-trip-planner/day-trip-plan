3eef2dfcb7dcefc0d39098cd71dc1588ff1d65dc
kaley forster <55896549+kale-redhead@users.noreply.github.com>
2021-06-23 16:45:16 -0400

    getting rid of useEffect

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom"
+import Cars from './Cars'
 
 const Products = () => {  
 
     return(
         <div>
             <h1>This is the products page</h1>
+            <Cars />
             <Link to='/hotels'>Hotel Rentals</Link>
             <Link to='/flights'>Flights</Link>
             <Link to='/cars'>Car Rentals</Link>
