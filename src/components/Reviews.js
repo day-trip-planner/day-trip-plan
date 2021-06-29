e6474c07393f0e7b68e3c67b5df6180b48f7d153
gengee323 <geng323@gmail.com>
2021-06-28 20:21:10 -0700

    review code

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,31 +1,41 @@
 import React from "react";
 	import StarRating from "./StarRating";
 
-	const Reviews = ({ reviews }) => {
-	  return (
-	    <div className="row row-cols-3 mb-2">
-	      {reviews.map((review) => {
-	        return (
-	          <div
-	            key={review.id}
-	            className="card text-white bg-primary mb-3 mr-4"
-	            style={{ maxWidth: "30%" }}
-	          >
-	            <div className="card-header d-flex justify-content-between">
-	              <span>{review.name}</span>
-	              <span>
-	                <StarRating rating={review.rating} />
-	              </span>
-	            </div>
-	            <div className="card-body">
-	              <p className="card-text">{review.review}</p>
-	            </div>
-	          </div>
-	        );
-	      })}
-	    </div>
-	  );
-	};
+	const Reviews = () => {
+		return (
+			<div>
+				<h1>This is the Reviews page
+	
+				</h1>
+			</div>
+		)
+	}
+	
+	
+// { reviews }
+	//     <div className="row row-cols-3 mb-2">
+	//       {reviews.map((review) => {
+	//         return (
+	//           <div
+	//             key={review.id}
+	//             className="card text-white bg-primary mb-3 mr-4"
+	//             style={{ maxWidth: "30%" }}
+	//           >
+	//             <div className="card-header d-flex justify-content-between">
+	//               <span>{review.name}</span>
+	//               <span>
+	//                 <StarRating rating={review.rating} />
+	//               </span>
+	//             </div>
+	//             <div className="card-body">
+	//               <p className="card-text">{review.review}</p>
+	//             </div>
+	//           </div>
+	//         );
+	//       })}
+	//     </div>
+	//   );
+	// };
 	
 
 	export default Reviews;
