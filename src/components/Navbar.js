c44a49a694dd9630b2cd0b8354f9a0a6630e85a2
Noe Pinedo <npine12@gmail.com>
2021-06-30 12:47:49 -0600

    logout button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,42 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
 import '../styles/Navbar.css'
 import ReorderIcon from '@material-ui/icons/Reorder'
 import SearchIcon from '@material-ui/icons/Search'
+import axios from 'axios'
+import { setUser } from '../redux/authReducer'
+import { setCart } from '../redux/cartReducer'
+import { useDispatch, useSelector } from 'react-redux'
+import { useHistory, Link } from 'react-router-dom'
 
-
-function Navbar() {
+function Navbar(props) {
 
     const [showLinks, setShowLinks] = useState(false);
+    const dispatch = useDispatch()
+    const history = useHistory()
+    const { user } = useSelector(state => state.auth)
+
+    const handleLogout = () => {
+        axios.delete('/auth/logout')
+            .then((res) => {
+                console.log('user logged out')
+                dispatch(setUser(null))
+                dispatch(setCart([]))
+                history.push('/')
+            })
+            .catch(err => console.log(err))
+    }
 
     return (
         <div className="Navbar">
             <div className="leftSide">
                 <div className="links" id={showLinks ? "hidden" : ""}>
                     <Link className="title" to='/'>The Day Tripper</Link>
-                    <Link to='/auth'>Login</Link>
+                    {!user ? <Link to='/auth'>Login</Link> : null}
                     <Link to='/products'>Packages</Link>
                     <Link to='/cart'>Cart</Link>
                     <Link to='/reviews'>Reviews</Link>
+                    {user ? <Link to='/' onClick={handleLogout}>Logout</Link> : null}
+
                 </div>
                 <button onClick={() => setShowLinks(!showLinks)}> <ReorderIcon /> </button>
             </div>
