3236bbf5286076b51804677d5c4b8d73c5e5c233
RobertLynch21 <r.f.lynch@gmail.com>
2021-06-30 14:50:42 -0400

    styled login form footer and images

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,12 +3,12 @@ import '../styles/footer.css'
 const Footer = () => {
     return (
         <div className="page-container">
-            <div className="content-wrap">
+            
                 <footer>
                     &copy;{new Date().getFullYear()} Day Trip Planner | All rights reserved | Terms of Use
 
                 </footer>
-            </div>
+            
         </div>
     )
 }
