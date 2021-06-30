3437db8cd8499fca2c4e9d761f7780c65de9386e
kaley forster <55896549+kale-redhead@users.noreply.github.com>
2021-06-30 10:55:48 -0400

    products are being a b

--- server/controllers/productsCtrl.js
+++ server/controllers/productsCtrl.js
@@ -2,27 +2,27 @@ module.exports = {
     getProducts: (req, res) => {
         const db = req.app.get('db')
 
-        db.products.get_products()
-        .then(products => {
-            res.status(200).send(products)
+        db.product.get_products()
+        .then(product => {
+            res.status(200).send(product)
         })
         .catch(err => {
             console.log(err)
             res.status(500).send(err)
         })
-    },
-
-    addProduct: (req, res) => {
-        const db = req.app.get('db')
-        const {product_id} = req.params
-    
-        db.product.add_product(product_id, name, price)
-            .then((product) => {
-                res.status(200).send(product)
-            })
-            .catch(err => {
-                console.log(err)
-                res.status(533).send(err)
-            })
     }
+
+    // addProduct: (req, res) => {
+    //     const db = req.app.get('db')
+    //     const {product_id} = req.params
+    
+    //     db.product.add_product(product_id, name, price)
+    //         .then((product) => {
+    //             res.status(200).send(product)
+    //         })
+    //         .catch(err => {
+    //             console.log(err)
+    //             res.status(533).send(err)
+    //         })
+    // }
 }
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,8 +1,71 @@
-const Products = () => {  
+import { useState, useEffect} from 'react'
+import axios from 'axios'
+import { useSelector, useDispatch } from 'react-redux'
+import {Link} from 'react-router-dom'
+import {setCart} from '../redux/cartReducer'
 
+const Products = (props) => {  
+    const [products, setProducts] = useState([])
+    const {user} = useSelector((store) => store.auth)
+    const {cart} = useSelector((store) => store.cartReducer)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        axios.get('/api/product')
+        .then((res) => {
+            setProducts(res.data) 
+        })
+        .catch(err => console.log(err + ' Products.js 18'))
+    }, [])
+
+    const handleAddToCart = (product_id) => {
+        const product = cart.find((product) => product.product_id === product_id)
+
+        if(!product){
+            axios.post(`/api/cart/${product_id}`)
+            .then((res) => {
+                dispatch(setCart(res.data))
+            })
+            .catch((err) => {
+                if(err.response.status === 511){
+                    props.history.push('/auth')
+                }
+            })
+        }
+        else{
+            axios.post(`/api/cart/${product_id}`, {quantity: product.quantity + 1})
+            .then((res) => {
+                dispatch(setCart(res.data))
+            })
+            .catch(err => {
+                console.log(err)
+                if(err.response.status === 511){
+                    props.history.push('/auth')
+                }
+            })
+        }
+    }
+
+    console.log(products)
     return(
         <div>
             <h1>This is the products page</h1>
+            {products.map((product) => {
+                return(
+                    <div key={product.product_id}>
+                        <div>{product.name}</div>
+                        <div>{product.price}</div>
+                        <div>{product.photo_one}</div>
+                        <div>{product.photo_two}</div>
+                        <div>{product.description}</div>
+                        {user &&
+                        <button onClick={() =>
+                        handleAddToCart(products.product_id)}>Add To Cart</button>}
+                    </div>
+                )
+            })}
+
+            <Link to='/cart'>View Cart</Link>
         </div>
     )
 }
