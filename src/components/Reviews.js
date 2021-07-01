e8995f04acbccafedab0a23c1e46a85ffde512ee
Genoveva Garcia <52330855+GenGee323@users.noreply.github.com>
2021-07-01 08:05:39 -0700

    new review

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,41 +1,13 @@
 import React from "react";
-// import StarRating from "./StarRating";
+import '../styles/reviews.css'
 
-	const Reviews = () => {
-		return (
-			<div>
-				<h1>Reviews
-				</h1>
-			</div>
-		)
-	}
-	
-	
-// { reviews }
-
-	//     <div className="row row-cols-3 mb-2">
-	//       {reviews.map((review) => {
-	//         return (
-	//           <div
-	//             key={review.id}
-	//             className="card text-white bg-primary mb-3 mr-4"
-	//             style={{ maxWidth: "30%" }}
-	//           >
-	//             <div className="card-header d-flex justify-content-between">
-	//               <span>{review.name}</span>
-	//               <span>
-	//                 <StarRating rating={review.rating} />
-	//               </span>
-	//             </div>
-	//             <div className="card-body">
-	//               <p className="card-text">{review.review}</p>
-	//             </div>
-	//           </div>
-	//         );
-	//       })}
-	//     </div>
-	//   );
-	// };
+const Review = () => {
+  return(
+    <div>
+      <h1>Review Page</h1>
+    </div>
+  )
+}
 
 
-	export default Reviews;
+export default Reviews;
