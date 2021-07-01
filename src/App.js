f2d21b8377c7e109a6e6f5642f599b0a0304e118
Noe Pinedo <npine12@gmail.com>
2021-06-30 18:24:40 -0600

    added img and review link

--- src/App.js
+++ src/App.js
@@ -8,8 +8,7 @@ function App() {
     <div className="App">
       <Navbar />
       {routes}
-      <p>Find the best travel deals on vacation packages when you book on Day Trip Planner. 24/7 Customer Support. Day Trip Planner Rewards. Get the Free Day Trip Planner App. Save up to 50% on Hotels. Verified Guest Reviews. Packages: Save up to 20% Double Points on App. Fast & Secure Booking.</p>
-     <Footer />
+      <Footer />
     </div>
   );
 }
