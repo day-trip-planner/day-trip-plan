4c687dec65ac9f66111f4a93b5900e728b7fe7a2
RobertLynch21 <r.f.lynch@gmail.com>
2021-07-02 11:40:56 -0400

    Fixed payment for error

--- src/components/StripeContainer.js
+++ src/components/StripeContainer.js
@@ -5,7 +5,7 @@ import { loadStripe } from "@stripe/stripe-js"
 import React from "react"
 import PaymentForm from "./PaymentForm"
 
-const PUBLIC_KEY = "pk_test_rgWMA3zxjAtwaB6iV8b5W40x"
+const PUBLIC_KEY = "pk_test_51J5XqEEBwtcHnvdzRTtHyCcWpqQWDNSpTM4WjPlBRVvXD4GmtVE7smKaLDd0Sj4LtxwQbe9LgCqEMIykBABK8wqf00USgKLjPU"
 
 const stripeTestPromise = loadStripe(PUBLIC_KEY)
 
